ba9c1f5b98a3dbb1e791e39638f768249ce04a46
Lucy Villogas <lvillogas.training@innevo.cl>
2025-10-30 17:04:26 -0500

    render

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -9,15 +9,16 @@ import { Video } from "../entities/Video";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "localhost",
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dropSchema: false, // Solo para desarrollo limpia la DB antes de crear las tablas
-  synchronize: true,  // solo para desarrollo, en producccion es false
-  logging: false,
+  ssl: { rejectUnauthorized: false },
   entities: [Inquiry, Plan, Project, User, Image, Video],
-  subscribers: [],
-  migrations: [],
+  synchronize: false,  // solo para desarrollo es true, en producccion es false
+  extra: {
+    connectionTimeoutMillis: 10000,
+    max: 5, // Límite de conexiones
+  },
 });
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import compression from "compression";
 import router from './routes/index';
 import errorHandler from './middleware/errorHandler';
 import morgan from 'morgan';
@@ -13,6 +14,9 @@ app.use(cors({
   credentials: true,
 }));
 app.use(morgan('dev'));
+app.use(compression());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(router);
 app.use(errorHandler);
 
