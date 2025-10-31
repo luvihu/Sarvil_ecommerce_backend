be669bec44cd9d14c9889d0e7aa445a1e2866f07
lucy villogas <luvillogas@gmail.com>
2025-10-31 15:59:19 -0500

    import

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -9,19 +9,50 @@ import { Video } from "../entities/Video";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false } // Render usa cert autofirmado en free tier
-    : false,
-  synchronize: false, // solo para desarrollo es true, en producccion es false
-  logging:false, 
-  entities: [Inquiry, Plan, Project, User, Image, Video],
-  extra: {
-    connectionTimeoutMillis: 10000,
-    max: 5, // Límite de conexiones
+  url: process.env.DATABASE_URL, // Render usa esta variable
+  ssl: {
+    rejectUnauthorized: false,
   },
+  synchronize: false,
+  logging: false,
+  entities: [Inquiry, Plan, Project, User, Image, Video],
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// import { DataSource } from "typeorm";
+// import "dotenv/config";
+// import { Inquiry } from "../entities/Inquiry";
+// import { Plan } from "../entities/Plan";
+// import { Project } from "../entities/Project";
+// import { User } from "../entities/User";
+// import { Image } from "../entities/Image";
+// import { Video } from "../entities/Video";
+
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: process.env.DB_HOST || "localhost",
+//   port: 5432,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   ssl: process.env.NODE_ENV === 'production'
+//     ? { rejectUnauthorized: false } // Render usa cert autofirmado en free tier
+//     : false,
+//   synchronize: false, // solo para desarrollo es true, en producccion es false
+//   logging:false, 
+//   entities: [Inquiry, Plan, Project, User, Image, Video],
+//   extra: {
+//     connectionTimeoutMillis: 10000,
+//     max: 5, // Límite de conexiones
+//   },
+// });
