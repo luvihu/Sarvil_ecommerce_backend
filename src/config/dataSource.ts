61a86e7d24dc4b3dc1a84ac4b2e37924bb805c12
sarvil360 <sarvil360solutions@gmail.com>
2025-10-30 21:28:37 -0500

    render deploy

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -9,11 +9,11 @@ import { Video } from "../entities/Video";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.POSTGRES_HOST || "localhost",
   port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   ssl: { rejectUnauthorized: false },
   synchronize: false,  // solo para desarrollo es true, en producccion es false
   entities: [Inquiry, Plan, Project, User, Image, Video],
