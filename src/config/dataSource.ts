6ef7eb600933f61fe9c0c555e6c8c1ca3270a9cd
lucy villogas <luvillogas@gmail.com>
2025-11-02 13:05:56 -0500

    run dev

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -7,26 +7,53 @@ import { User } from "../entities/User";
 import { Image } from "../entities/Image";
 import { Video } from "../entities/Video";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL, // Render usa esta variable
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  synchronize: false,
+  ...(isProduction 
+    ? {
+      url: process.env.DATABASE_URL, 
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: !isProduction,
+    }
+  : {
+    host: process.env.DB_HOST || "localhost",
+    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: false,
+    synchronize:true
+  }
+  ),
   logging: false,
   entities: [Inquiry, Plan, Project, User, Image, Video],
 });
 
 
+// import { DataSource } from "typeorm";
+// import "dotenv/config";
+// import { Inquiry } from "../entities/Inquiry";
+// import { Plan } from "../entities/Plan";
+// import { Project } from "../entities/Project";
+// import { User } from "../entities/User";
+// import { Image } from "../entities/Image";
+// import { Video } from "../entities/Video";
 
 
-
-
-
-
-
-
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   url: process.env.DATABASE_URL, // Render usa esta variable
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   synchronize: false,
+//   logging: false,
+//   entities: [Inquiry, Plan, Project, User, Image, Video],
+// });
 
 
 // import { DataSource } from "typeorm";
